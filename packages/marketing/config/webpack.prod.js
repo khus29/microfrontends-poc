ed2f50f7d7da83b68a91b79e741e25924d1bbf48
Khushboo Kumari <khukumar@publicisgroupe.net>
2021-08-01 17:55:39 +0530

    prod build updates

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,20 +1,24 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
 
-const prodConfig = {
+const devConfig = {
   mode: 'production',
-  devServer: {
-    port: 8081,
-    historyApiFallback: {
-      index: 'index.html',
-    },
+  output: {
+    filename: '[name].[contenthash].js',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, prodConfig);
+module.exports = merge(commonConfig, devConfig);
